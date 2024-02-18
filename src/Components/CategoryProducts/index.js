b347449ee5f87c371ef15794977a8fb2fd4386e8
Abhiramnanabolu <abhiramnanabolu0706@gmail.com>
2024-02-18 19:27:46 +0530

    Update index.js

--- src/Components/CategoryProducts/index.js
+++ src/Components/CategoryProducts/index.js
@@ -7,7 +7,19 @@ class CategoryProducts extends Component{
     
     componentDidMount() {
         this.getCategoryProducts()
+        this.getSubCategories()
     }
+
+    filterObjectsWithSpecialChars=(objects)=> {
+          // Filter out objects where subcategory contains / or "
+          const filteredObjects = objects.filter((obj) => {
+            const subcategoryValue = obj.subCategory || ''; // Handle cases where subCategory is undefined
+            return !subcategoryValue.includes('\"'); // Check for backslash '\'
+          });
+        
+          return filteredObjects;
+      }
+      
 
     getCategoryProducts = async () => {
         const {match} = this.props
@@ -42,29 +54,77 @@ class CategoryProducts extends Component{
           }
       }
 
+      getSubCategories = async () => {
+        const {match} = this.props
+        const {params} = match
+        const {category} = params
+        this.setState({cat:category})
+        const subCat="Cucumber & Capsicum,Root Vegetables"
+        try {
+            const response = await fetch(`http://localhost:3001/ec/subcategories/${category}`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+          
+            if (response.ok) {
+              const subcategories = await response.json();
+              const finalSubCategories=this.filterObjectsWithSpecialChars(subcategories)
+              console.log("Sub-Categories:", finalSubCategories);
+              this.setState({subCategories:finalSubCategories})
+            } else {
+              const errorData = await response.json();
+              console.error("Error fetching products:", errorData.error);
+          
+            }
+          } catch (error) {
+            console.error("Error during products fetch:", error.message);
+          
+          }
+      }
+
 
     render(){
-        const {productCount,cat}=this.state
+        const {productCount,cat,subCategories}=this.state
         return(
             <div className="flex flex-col h-screen font-[Poppins]">
                 <Header />
-                <div className="p-3 flex flex-row">
-                    <Link to={`/`} className="font-semibold text-base">Home</Link>
-                    <p className="mx-2 text-base">/</p>
-                    <p className="text-base">{cat}</p>
+                <div className="px-8 pt-4 flex flex-row text-slate-700 text-sm">
+                    <Link to={`/`} className="font-bold underline underline-offset-4">Home</Link>
+                    <p className="mx-2 text-bas">/</p>
+                    <p className="underline underline-offset-4">{cat}</p>
+                </div>
+                <div className="px-8 py-3">
+                    <h1 className="font-bold text-2xl">{`${cat} (${productCount})`}</h1>
                 </div>
                 <div className="flex-1 flex overflow-hidden">
-                    <div className="w-2/12 overflow-auto p-4">
+                    <div className="w-3/12 overflow-auto p-4">
                         {/* Content for the left div */}
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent
-                            libero. Sed cursus ante dapibus diam. Sed nisi.
-                        </p>
+                        <div className="px-4 mt-2">
+                            <p className="font-medium">Shop By Sub-Category</p>
+                            <ul className="list-none mt-4">
+                                {subCategories.map(eachItem=>(
+                                    <li
+                                    className="flex mb-2"
+                                    key={eachItem.subCategory}
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      className=" m0p0"
+                                      value={eachItem.subCategory}
+                                      onChange={this.onChange1}
+                                    />
+                                    <p className="text-slate-600 ml-3 text-sm">{eachItem.subCategory}</p>
+                                  </li>
+                                ))}
+                            </ul>
+                        </div>
                         {/* Add more content as needed */}
                     </div>
-                    <div className="w-10/12 overflow-auto p-4">
+                    <div className="w-9/12 overflow-auto p-4">
                         {/* Content for the right div */}
-                        <div>
+                        <div className="mt-2">
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent
                             libero. Sed cursus ante dapibus diam. Sed nisi.
